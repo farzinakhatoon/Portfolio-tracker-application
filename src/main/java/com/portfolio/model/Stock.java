754d281dba412e6991921915dc6f8934e6e472f3
farzinakhatoon <136377484+farzinakhatoon@users.noreply.github.com>
2024-12-25 13:28:29 +0530

    Update Stock.java

---
+++ src/main/java/com/portfolio/model/Stock.java
@@ -0,0 +1,52 @@
+package com.portfolio.controller;
+
+import com.portfolio.model.Stock;
+import com.portfolio.service.StockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/stocks")
+public class StockController {
+
+    @Autowired
+    private StockService stockService;
+
+    // Add a new stock
+    @PostMapping
+    public Stock addStock(@RequestBody Stock stock) {
+        return stockService.addStock(stock);
+    }
+
+    // Get all stocks
+    @GetMapping
+    public List<Stock> getAllStocks() {
+        return stockService.getAllStocks();
+    }
+
+    // Get stock by ticker
+    @GetMapping("/{ticker}")
+    public Stock getStock(@PathVariable String ticker) {
+        return stockService.getStockByTicker(ticker);
+    }
+
+    // Update stock information
+    @PutMapping("/{ticker}")
+    public Stock updateStock(@PathVariable String ticker, @RequestBody Stock stock) {
+        return stockService.updateStock(ticker, stock);
+    }
+
+    // Delete stock by ticker
+    @DeleteMapping("/{ticker}")
+    public void deleteStock(@PathVariable String ticker) {
+        stockService.deleteStock(ticker);
+    }
+
+    // Get total portfolio value
+    @GetMapping("/portfolio-value")
+    public double getTotalPortfolioValue() {
+        return stockService.getTotalPortfolioValue();
+    }
+}
