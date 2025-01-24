ee0464ea1ef2f7ee6a4e421ce063eca799704859
farzinakhatoon <136377484+farzinakhatoon@users.noreply.github.com>
2025-01-24 17:16:44 +0530

    Update StockController.java

--- src/main/java/com/portfolio/controller/StockController.java
+++ src/main/java/com/portfolio/controller/StockController.java
@@ -11,45 +11,39 @@ import java.util.List;
 @RequestMapping("/api/stocks")
 public class StockController {
 
+    @Autowired
+    private StockService stockService;
+
     @GetMapping("/")
     public String welcome() {
         return "Welcome to the Portfolio Tracker API!";
     }
 
-    @Autowired
-    private StockService stockService;
-
-    // Add a new stock
     @PostMapping
     public Stock addStock(@RequestBody Stock stock) {
         return stockService.addStock(stock);
     }
 
-    // Get all stocks
     @GetMapping
     public List<Stock> getAllStocks() {
         return stockService.getAllStocks();
     }
 
-    // Get stock by ticker
     @GetMapping("/{ticker}")
     public Stock getStock(@PathVariable String ticker) {
         return stockService.getStockByTicker(ticker);
     }
 
-    // Update stock information
     @PutMapping("/{ticker}")
     public Stock updateStock(@PathVariable String ticker, @RequestBody Stock stock) {
         return stockService.updateStock(ticker, stock);
     }
 
-    // Delete stock by ticker
     @DeleteMapping("/{ticker}")
     public void deleteStock(@PathVariable String ticker) {
         stockService.deleteStock(ticker);
     }
 
-    // Get total portfolio value
     @GetMapping("/portfolio-value")
     public double getTotalPortfolioValue() {
         return stockService.getTotalPortfolioValue();
